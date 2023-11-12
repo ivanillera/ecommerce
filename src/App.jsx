603c5b3fc7234730817631c3e4c513027335478d
Ivan <ivanillera@gmail.com>
2023-11-12 12:31:58 -0300

    products come from backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Products/>
-      <Footer/>
-    </>
-  )
+      <div className="flex-grow">
+        <Products />
+      </div>
+      <Footer className="fixed bottom-0" />
+    </div>
+  );
 }
 
 export default App
