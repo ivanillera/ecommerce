898d0c9a10d7fa9fdb475a576313facb873c5699
Ivan <ivanillera@gmail.com>
2023-11-27 13:59:43 -0300

    update README.md and App.jsx

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # React + Vite
-
+Para iniciar frontend es npm run dev
+Para iniciar backend es node .\index.js
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Products from "./components/Products"
 import Footer from "./components/Footer"
 
 function App() {
+  const currentMonth = new Date().getMonth();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <div className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center pt-10">
+        Ofertas para el mes {currentMonth}
+      </div>
       <div className="flex-grow">
         <Products />
       </div>
